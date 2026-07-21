1bb3ce5f0d58185fe5c9c8560bee4cbe30218496
Diego <ctapls2021@gmail.com>
2026-07-20 21:43:09 -0300

    Revert "feat: replace all project gallery images with clean versions and restore HTML text overlays for consistent text rendering sharpness"
    This reverts commit d676fd6ce063a0891c529eba10250870a7790bd2.

--- scratch/find_text.py
+++
@@ -1,6 +0,0 @@
-with open('src/components/Process.astro', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-import re
-for match in re.finditer(r'<[^>]*class=[^>]*textContents[^>]*>(.*?)</[^>]*>', content):
-    print(match.group(0))
